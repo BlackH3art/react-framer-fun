12abf08b4ab5c9a7f0c07fdbb705cf57b41f4392
BlackH3art <Jaroslaw.Musielak@outlook.com>
2020-11-29 21:20:17 +0100

    5th component - basic validation

--- src/components/FifthComponent.jsx
+++ src/components/FifthComponent.jsx
@@ -61,37 +61,43 @@ const FifthComponent = () => {
 
             <label>
               my input
+              {errors.text ? <p className="validate-error">{errors.text}</p> : null}
             </label>
             <input name="text" type="text" value={formState.text} onChange={handleOnChange}/>
-            <p>{JSON.stringify(errors)}</p>
 
             <label>
               my email input
+              {errors.email ? <p className="validate-error">{errors.email}</p> : null}
             </label>
             <input name="email" type="text" value={formState.email} onChange={handleOnChange}/>
 
             <label>
               my number input
+              {errors.number ? <p className="validate-error">{errors.number}</p> : null}
             </label>
             <input name="number" type="number" value={formState.number} onChange={handleOnChange}/>
 
             <label>
               my checkbox input
+              {errors.checkbox1 ? <p className="validate-error">{errors.checkbox1}</p> : null}
             </label>
             <input name="checkbox1" className="checkbox" type="checkbox" onChange={handleCheckbox} checked={formState.checkbox1} />
 
             <label>
-              my checkbox input #2
+              my checkbox input2
+              {errors.checkbox2 ? <p className="validate-error">{errors.checkbox2}</p> : null}
             </label>
             <input name="checkbox2" className="checkbox" type="checkbox" onChange={handleCheckbox} checked={formState.checkbox2} />
 
             <label>
               my date input
+              {errors.date ? <p className="validate-error">{errors.date}</p> : null}
             </label>
             <input name="date" type="date" value={formState.date} onChange={handleOnChange}/>
 
             <label>
               my time input
+              {errors.time ? <p className="validate-error">{errors.time}</p> : null}
             </label>
             <input name="time" type="time" value={formState.time} onChange={handleOnChange}/>
 
--- src/components/store/validationMiddleware.js
+++ src/components/store/validationMiddleware.js
@@ -1,28 +1,56 @@
 import validator from 'validator';
 
-const errors = [];
+const errors = {
+  text: [],
+  email: [],
+  number: [],
+  checkbox1: [],
+  checkbox2: [],
+  date: [],
+  time: []
+};
 
 export const validation = ({ dispatch, getState }) => next => action => {
 
   // Object.keys(action.payload).forEach(field => {
-  //   let fieldError = [];
   //   let value = action.payload[field];
 
+  //   console.log(String(value));
+
   //   if (validator.isEmpty(value)) {
-  //     fieldError.push("value is required!")
+  //     errors[field].push("value is required!")
   //   }
+
+  //   console.log(errors);
 
 
   // })
 
+  const { text, email, number, checkbox1, checkbox2, date, time } = action.payload
 
-  if(validator.isEmpty(action.payload.text)) {
 
-    errors.push('value is required!')
-
+  if(validator.isEmpty(text)) {
+    errors.text.push('value is required!')
+  } else if (!validator.isLength(text, {min: 5, max: 30})) {
+    errors.text.push('min 5 signs, and max30 signs')
+  } else if (!validator.isEmail(email)){
+    errors.email.push('this is not an email')
+  } else if (!validator.isNumeric(number)){
+    errors.number.push('this is not a number')
+  } else if(!checkbox1){
+    errors.checkbox1.push('you have to agree');
+  } else if(!checkbox2){
+    errors.checkbox2.push('you have to agree');
+  } else if(!validator.isDate(date)){
+    errors.date.push('this is not a date');
+  } else if(!validator.isLength(time, {min: 4})){
+    errors.time.push('this is not a correct time');
   } else {
     next(action)
   }
+
+  // next(action)
+
 }
 
 
